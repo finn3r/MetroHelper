cf20a9d1573e78fa7a437af50833338d70037d4d
Tsvetkov Ivan <finnerofficial@gmail.com>
2022-06-26 16:16:25 +0300

    Change ClearButton on input

--- src/components/Menu/Input/index.tsx
+++ src/components/Menu/Input/index.tsx
@@ -1,12 +1,20 @@
 import React, {useState, useMemo, KeyboardEvent} from 'react';
 import * as ST from '../../../styled';
+import {ReactComponent as ClearButton} from "../../../svg/ClearButton.svg";
 import {ReactComponent as CircleInput} from "../../../svg/CircleInput.svg";
 import {IInput} from "../../../interfaces/IInput";
 import searchOptions from "../../../scripts/searchOptions"
 import AutoComplete from "./AutoComplete";
 import {useAppSelector} from "../../../hooks/redux";
 
-const Index = React.forwardRef<HTMLInputElement, IInput>(({value, changeValue, placeHolder, options, focused, changeFocus}, ref) => {
+const Index = React.forwardRef<HTMLInputElement, IInput>(({
+                                                              value,
+                                                              changeValue,
+                                                              placeHolder,
+                                                              options,
+                                                              focused,
+                                                              changeFocus
+                                                          }, ref) => {
     const {stations} = useAppSelector(state => state.city);
     const [select, setSelect] = useState(0);
 
@@ -29,12 +37,14 @@ const Index = React.forwardRef<HTMLInputElement, IInput>(({value, changeValue, p
     };
 
     return (
-        <ST.InputContainer data-type={placeHolder}>
+        <ST.InputContainer className={focused ? "focused" : ""}>
             <ST.InputCircle style={{gridArea: "circle"}} className={focused ? "focused" : ""}>
                 <CircleInput fill={stations[options?.includes(value) ? value : ""] ?? "rgba(0, 0, 0, 0.5)"}/>
             </ST.InputCircle>
-            <ST.InputFieldContainer className={focused ? "focused" : ""}>
+            <ST.InputFieldContainer>
                 <ST.InputField
+                    type={"text"}
+                    autoComplete={"off"}
                     ref={ref}
                     spellCheck={false}
                     placeholder={placeHolder}
@@ -43,24 +53,24 @@ const Index = React.forwardRef<HTMLInputElement, IInput>(({value, changeValue, p
                     onKeyDown={handleSelect}
                     onChange={(e) => changeValue(e.target.value)}
                 />
-                <ST.InputUnFocusButton>
-                    <ST.InputUnFocusButtonText
-                        onClick={() => changeFocus ? changeFocus(false) : null}
-                    >
-                        Отмена
-                    </ST.InputUnFocusButtonText>
-                </ST.InputUnFocusButton>
                 <AutoComplete
-                    options={value ? searchedOptions: []}
+                    options={value ? searchedOptions : []}
                     hidden={!value}
                     changeValue={changeValue}
                     changeSelected={setSelect}
                     selected={select}
                 />
-                <ST.InputClearButton hidden={!value} onClick={() => changeValue("")}>
-                    x
-                </ST.InputClearButton>
             </ST.InputFieldContainer>
+            <ST.InputUnFocusButton>
+                <ST.InputUnFocusButtonText
+                    onClick={() => changeFocus ? changeFocus(false) : null}
+                >
+                    Отмена
+                </ST.InputUnFocusButtonText>
+            </ST.InputUnFocusButton>
+            <ST.InputClearButton hidden={!value} onClick={() => changeValue("")}>
+                <ClearButton/>
+            </ST.InputClearButton>
         </ST.InputContainer>
     );
 });
